3304a149b1bdc7b2895b58c424abeceb0c8f6db2
Jared Blitzstein <mailing-list@blitzstein.net>
2021-11-08 09:01:44 -0500

    Updated with default message

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import "./App.css"
+import Typography from '@mui/material/Typography'
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
+class WokeTip extends Component {
+  
+    render() {
+  
     return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
+      <Typography variant="h2" component="h2">
+          If you are upset the keyboard is QUERTY and not ABC, you might be a boomer.
+      </Typography>
     )
   }
 }
@@ -35,12 +18,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
+        <header className="App-header">          
+          <WokeTip/>
         </header>
       </div>
     )
